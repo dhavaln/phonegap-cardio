a4c9b2e207634f53b9cb710e563c7bf64d5f07f5
dhaval nagar <dhaval.b.nagar@gmail.com>
2012-03-24 12:04:17 +0530

    change is the activity

--- src/com/jumpbyte/android/cardio/App.java
+++ src/com/jumpbyte/android/cardio/App.java
@@ -30,62 +30,6 @@ public class App extends DroidGap {
 		super.loadUrl("file:///android_asset/www/index.html");
 	}
 	
-    public void onCreate1(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        super.setContentView(R.layout.main);
-        
-     // Find the scan button and setup an onClickListener
-        Button scanButton = (Button) findViewById(R.id.chargeCreditCardButton);
-        scanButton.setOnClickListener(new Button.OnClickListener() {
-        	@Override
-        	public void onClick(View v) {
-        		// TODO Auto-generated method stub
-        		Intent intent = new Intent(App.this, CardIOActivity.class);
-
-                // EXTRA_APP_TOKEN - required
-                // (failing to include this will cause an exception)
-                intent.putExtra(CardIOActivity.EXTRA_APP_TOKEN, "6a32bd3378404e978ec66854583d4626");
-
-                // Create an example "shopping cart" here. Your code will likely be much more complex.
-                List<ChargeableItem> myCart = new ArrayList<ChargeableItem>();
-                myCart.add(new ChargeableItem("A one dollar item", 100));
-
-                // Create the charge request with parameters:
-                // 1. our example cart
-                // 2. currency (currently only "USD" supported)
-                // 3. whether the charge is live (you'll probably want to keep this false during development)
-                ChargeRequest myRequest = new ChargeRequest(myCart, "USD", false);
-                intent.putExtra(CardIOActivity.EXTRA_CHARGE_REQUEST, myRequest);
-
-                BroadcastReceiver br = new BroadcastReceiver(){
-                	@Override
-                	public void onReceive(Context context, Intent intent) {
-                		// TODO Auto-generated method stub
-                		Log.i("RESULT BROADCAST", "got result: "
-                                + intent.getParcelableExtra(CardIOActivity.EXTRA_CHARGE_RESULT));
-
-                            // TODO: deliver purchased item.
-
-                            // Receiver will be leaked if it is not unregistered.
-                            // There should only be one active receiver, so this is a good place to unregister it.
-                            unregisterReceiver(this);
-                	}
-                    
-                };
-                
-                // Register the broadcast receiver.
-                // IMPORTANT: purchased items MUST be delivered in response to this broadcast.
-                // It is possible for the charge to complete without the user pressing "Done".
-                App.this.registerReceiver(br, new IntentFilter(CardIOActivity.ACTION_CHARGE_COMPLETE)
-                );
-
-                // Start the activity. We're using 1 for our request code,
-                // but you can use whatever value makes sense.
-                App.this.startActivityForResult(intent, 1);
-        	}
-        });
-    }
-
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		super.onActivityResult(requestCode, resultCode, data);
